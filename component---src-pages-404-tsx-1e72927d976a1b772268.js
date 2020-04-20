621aef321a13b248927f4ff65210871fe4bfcfbb
Jo Sprague <josiah.sprague@gmail.com>
2020-04-20 22:47:15 +0100

    Updates

---
+++ component---src-pages-404-tsx-1e72927d976a1b772268.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(t,e,n){"use strict";n.r(e);n("q1tI");var c=n("NqE+"),l=n("TBFr"),a=n("+ego"),d=n("Txvj"),o=n("mbD7"),u=n("qKvR");e.default=function(){return Object(u.d)(a.a,null,Object(u.d)(c.a,null,Object(u.d)(l.a,null,Object(u.d)(d.a,{title:"/.welcome"},Object(u.d)(o.a,{text:"404",large:!0}),Object(u.d)("p",null,"command not found")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1e72927d976a1b772268.js.map
