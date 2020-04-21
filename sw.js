2065b3ca8ff5aa7437174cc5ed7770a46ea96f15
Jo Sprague <josiah.sprague@gmail.com>
2020-04-21 14:37:37 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ee9f49ebd3ea152edb94.js"
+    "url": "webpack-runtime-64624c10479790576360.js"
   },
   {
     "url": "styles.6a833ee5dc617fa01521.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4b29f70cae6a39dac65d13bdc9c2c009"
+    "revision": "7149a8c34ae6adc1e6cee5eca496aeb4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
