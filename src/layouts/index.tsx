e2c239f259984e54b0d63bd0cb5d45d43d8e96a3
Jo Sprague <josiah.sprague@gmail.com>
2020-05-04 19:32:25 +0100

    Add per-page meta tags support

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
+import React, { ReactNode } from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import 'modern-normalize'
 import '../styles/normalize'
@@ -8,25 +8,20 @@ import '../styles/normalize'
 import LayoutRoot from '../components/LayoutRoot'
 import LayoutMain from '../components/LayoutMain'
 
-interface StaticQueryProps {
-  site: {
-    siteMetadata: {
-      title: string
-      description: string
-      keywords: string
-      siteUrl: string
-      siteImage: string
-      author: {
-        name: string
-      }
-    }
-  }
+interface ISEO {
+  children?: ReactNode
+  description?: string
+  lang?: string
+  meta?: []
+  title?: string
+  image?: string
+  pathname?: string
 }
 
-const IndexLayout: React.FC = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query IndexLayoutQuery {
+const IndexLayout: React.FC<ISEO> = ({ children, description = '', lang = 'en', meta = [], image, title, pathname }: ISEO) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
         site {
           siteMetadata {
             title
@@ -40,29 +35,35 @@ const IndexLayout: React.FC = ({ children }) => (
           }
         }
       }
-    `}
-    render={(data: StaticQueryProps) => (
-      <LayoutRoot>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: data.site.siteMetadata.description },
-            { name: 'keywords', content: data.site.siteMetadata.keywords },
-            { name: 'author', content: data.site.siteMetadata.author.name },
-            { property: 'og:title', content: data.site.siteMetadata.title },
-            { property: 'og:description', content: data.site.siteMetadata.description },
-            { property: 'og:image', content: data.site.siteMetadata.siteImage },
-            { property: 'og:url', content: data.site.siteMetadata.siteUrl },
-            { name: 'twitter:title', content: data.site.siteMetadata.title },
-            { name: 'twitter:description', content: data.site.siteMetadata.description },
-            { name: 'twitter:image', content: data.site.siteMetadata.siteImage },
-            { name: 'twitter:card', content: 'summary_large_image' }
-          ]}
-        />
-        <LayoutMain>{children}</LayoutMain>
-      </LayoutRoot>
-    )}
-  />
-)
+    `
+  )
+  const metaTitle = title ? `${title} | ${site.siteMetadata.title}` : site.siteMetadata.title
+  const metaDescription = description || site.siteMetadata.description
+  const metaImage = image ? `${site.siteMetadata.siteUrl}${image}` : site.siteMetadata.siteImage
+  const canonical = pathname ? `${site.siteMetadata.siteUrl}${pathname}` : null
+  return (
+    <LayoutRoot>
+      <Helmet
+        htmlAttributes={{ lang }}
+        title={metaTitle}
+        link={canonical ? [{ rel: 'canonical', href: canonical }] : []}
+        meta={[
+          { name: 'description', content: metaDescription },
+          { name: 'keywords', content: site.siteMetadata.keywords },
+          { name: 'author', content: site.siteMetadata.author.name },
+          { property: 'og:title', content: metaTitle },
+          { property: 'og:description', content: metaDescription },
+          { property: 'og:image', content: metaImage },
+          { property: 'og:url', content: site.siteMetadata.siteUrl },
+          { name: 'twitter:title', content: site.siteMetadata.title },
+          { name: 'twitter:description', content: metaDescription },
+          { name: 'twitter:image', content: metaImage },
+          { name: 'twitter:card', content: 'summary_large_image' }
+        ].concat(meta)}
+      />
+      <LayoutMain>{children}</LayoutMain>
+    </LayoutRoot>
+  )
+}
 
 export default IndexLayout
